26fdadde9db4e62e930bab2d1d9ee95887786969
Vedant Palekar <75372283+hoopinwhoopin@users.noreply.github.com>
2024-07-19 19:46:04 +0530

    chan

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
       <div className=" mt-auto ml-auto text-yellow-300 flex-col flex 2xl:pr-[100px]  ">
         <div className="Boldy flex flex-row">
           LOVE{" "}
-          <p className="Actonia lg:text-5xl 2xl:text-[64px] text-3xl  text-nowrap text-white ml-2 mr-2 2xl:mt-4 lg:mt-2 mt-4">
+          <p className="Actonia lg:text-5xl 2xl:text-[64px] text-3xl  text-nowrap text-white ml-2 mr-2 2xl:mt-4 lg:mt-2 mt-2">
             {" "}
             is the{" "}
           </p>                            
