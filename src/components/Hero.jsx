0e88e7ce0f090e86b1916d8b854e9db3a1569d82
Vedant Palekar <75372283+hoopinwhoopin@users.noreply.github.com>
2024-07-19 18:33:07 +0530

    chan

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
 function Hero() {
   return (
-    <div className="bg-hero-pattern h-screen flex flex-auto bg-cover justify-center items-center text-white lg:text-5xl 2xl:text-[70px] text-4xl  bg-top px-20 pb-12">
+    <div className="bg-hero-pattern h-screen flex flex-auto bg-cover justify-center items-center text-white lg:text-5xl 2xl:text-[70px] text-[35px]  bg-top px-20 pb-12">
       <div className=" mt-auto ml-auto text-yellow-300 flex-col flex 2xl:pr-[100px]  ">
         <div className="Boldy flex flex-row">
           LOVE{" "}
           <p className="Actonia lg:text-5xl 2xl:text-[64px] text-3xl  text-nowrap text-white ml-2 mr-2 2xl:mt-4 mt-2">
             {" "}
             is the{" "}
-          </p>
+          </p>                            
           <span className=" text-nowrap Boldy text-white ">WHOLE THING.</span>
         </div>
         <div className="Boldy text-white text-end 2xl:leading-[55px] leading-[15px] pb-4">
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -13,7 +13,7 @@ function Slider() {
   }, []);
 
   return (
-    <div className="sm:pt-[100px] pt 2xl:pt-[125px] bg-[#EDECE7] p-5 justify-self-end  w-full 2xl:h-[954px]  overflow-hidden ">
+    <div className="pt-[50px] pt 2xl:pt-[125px] bg-[#EDECE7] p-5 justify-self-end  w-full 2xl:h-[954px]  overflow-hidden ">
       <div className="flex flex-col sm:flex-row  items-start sm:items-center justify-center ">
       <div className="flex flex-row text-4xl justify-end mr-3 ">
           <div className="flex flex-row">
@@ -23,7 +23,7 @@ function Slider() {
           </div>
           <span className="text-[#1C1C1C] Boldy text-[40px] sm:text-[48px] 2xl:text-[70px] mt-4 mr-10 ">Stories</span>
         </div>
-        <div className="Courier Prime text-[16px] 2xl:text-[20px] sm:text-[16px] font-[#4A4A4A] max-w-4xl mr-10">
+        <div className="Courier Prime text-[16px] 2xl:text-[20px] sm:text-[16px] font-[#4A4A4A] max-w-4xl mr-10 pt-[35px]">
           are different and every couple is unique. Your moments become your
           life story and live from one generation to the next. Here are your
           stories through our particular way of seeing it
