0a70f97775f6b10e7f98e528ad7b67d664d47279
situn <chiranjivrao37@gmail.com>
2024-07-20 13:06:20 +0530

    fix pagination

--- src/pages/Photostoriessingle.jsx
+++ src/pages/Photostoriessingle.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import Pagination from "@mui/material/Pagination";
 import PaginationItem from "@mui/material/PaginationItem";
 import Stack from "@mui/material/Stack";
+import facebook_icon from "../assets/FacebookLogo.png";
+import insta_icon from "../assets/InstagramLogo.png";
 import ArrowCircleLeftOutlinedIcon from "@mui/icons-material/ArrowCircleLeftOutlined";
 import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
 import useFetchData from "../customHooks/useFetchData";
@@ -28,7 +30,28 @@ function Photostoriessingle() {
           backgroundImage: `url(${photoStorySingleData?.image})`,
         }}
       />
-      <div className="bg-[#EDECE7] flex justify-center items-center">
+      <div className="text-center text-[70px] Boldy text-[#1C1C1C] my-[60px]">
+        alison + joel
+      </div>
+      <div className="flex px-[8%] gap-4 mb-[30px]">
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Interdum
+          velit euismod in pellentesque massa placerat duis ultricies lacus.
+          Massa tincidunt dui ut ornare lectus sit amet est placerat.
+          Consectetur purus ut faucibus pulvinar elementum. Dolor sit amet
+          consectetur adipiscing elit pellentesque.
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Interdum
+          velit euismod in pellentesque massa placerat duis ultricies lacus.
+          Massa tincidunt dui ut ornare lectus sit amet est placerat.
+          Consectetur purus ut faucibus pulvinar elementum. Dolor sit amet
+          consectetur adipiscing elit pellentesque.
+        </div>
+      </div>
+      <div className="bg-[#EDECE7] flex justify-center items-center pb-[150px]">
         <div className="flex flex-col items-center w-[85%]">
           <div className="text-5xl 2xl:text[70px] uppercase Boldy text-[#1c1c1c] text-center pt-10 pb-10">
             {photoStorySingleData?.title}
@@ -41,20 +64,41 @@ function Photostoriessingle() {
               {photoStorySingleData?.descriptionRight}
             </div>
           </div>
-          {/* <div className="w-full">
+          <div className="w-full">
             <img
-              src={photoStorySingleData?.mainImage}
+              src="https://cdn.sanity.io/images/u8qx4arf/production/549fc0248ea993a73060723b5156991511165c55-1594x20805.jpg"
               alt="placeholder"
               className="w-full h-auto"
             />
-          </div> */}
-          <div className="h-[100px]">
+          </div>
+          <div className=" w-full mt-[60px]">
+            <div className=" bg-[#4A4A4A] h-[1.5px] border-[#4A4A4A] w-full border " />
+            <div className="flex justify-between my-[70px]">
+              <div className="w-[367px] text-[#1C1C1C] text-[18px] uppercase text-opacity-40">
+                wedding, destination, photography, blog, experimental
+              </div>
+              <div className="flex text-[#1C1C1C] gap-[10px]">
+                <a
+                  href="https://facebook.com/FlashbakcStudios/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img className="pr-1" src={facebook_icon} alt="" />
+                </a>
+                <a
+                  href="https://www.instagram.com/flashbakcstudios"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img className="pr-1" src={insta_icon} alt="" />
+                </a>
+              </div>
+            </div>
+
             <div className=" bg-[#4A4A4A] h-[1.5px] border-[#4A4A4A] w-full border " />
           </div>
           {photoStoryNextPrevious != null ? (
             <div className="w-full mt-10 pb-2">
-              {console.log(photoStoryNextPrevious)}
-              <div className=" bg-[#4A4A4A] h-[1.5px] border-[#4A4A4A] w-full border " />
               <div className="w-full justify-between flex flex-row">
                 {photoStoryNextPrevious[0] != null ? (
                   <div className="flex items-end flex-row-reverse">
@@ -95,45 +139,15 @@ function Photostoriessingle() {
               </div>
             </div>
           ) : null}
-          {/* <div className="Courier pb-[50px] flex justify-center bg-green-400">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 lg:gap-30 max-w-full">
-              {photoStoriesData.map((photostory, index) => (
-                <div className="flex justify-center mr-2">
-                  <PhotoStoryTile
-                    name={`${photostory?.firstPersonName} + ${photostory?.secondPersonName}`}
-                    location={photostory.state}
-                    img={photostory?.image}
-                    link={photostory?.narrativeLink}
-                  />
-                </div>
-              ))}
-            </div>
-          </div> */}
-
-          {/* <Stack spacing={2} className="pt-6 pb-12">
-            <Pagination
-              count={10}
-              renderItem={(item) => (
-                <PaginationItem
-                  slots={{
-                    previous: ArrowCircleLeftOutlinedIcon,
-                    next: ArrowCircleRightOutlinedIcon,
-                  }}
-                  // eslint-disable-next-line react/jsx-props-no-spreading
-                  {...item}
-                />
-              )}
-            />
-          </Stack> */}
         </div>
       </div>
-      <div className="h-[450px] sm:h-full flex justify-center items-center overflow-hidden">
+      {/*<div className="h-[450px] sm:h-full flex justify-center items-center overflow-hidden">
         <img
           src="https://cdn.sanity.io/images/u8qx4arf/production/719b9927c0c61f58c77df7c4c9ddaca3af3a715d-2560x920.png"
           className="object-cover h-[450px] sm:h-full w-full"
           alt="Quote"
         />
-      </div>
+      </div>*/}
     </div>
   );
 }
